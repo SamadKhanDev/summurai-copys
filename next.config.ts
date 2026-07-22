981069b3bc6e96f6c1e5ea918b672094111e6d0d
Abdus Samad Khan <abdussamadkhan026@gmail.com>
2026-07-22 15:49:30 +0500

    error resolved

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 import { BASE_PATH } from "./lib/basePath";
+import path from "path";
 
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
   basePath: BASE_PATH,
   env: {
     NEXT_PUBLIC_APP_BASE_PATH: BASE_PATH,
